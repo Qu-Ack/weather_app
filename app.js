1a4cc0a0a7f5ce8efd13dc483b0c5daf9b617f24
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2023-07-31 12:27:29 +0530

    I forgot to commit sorry but the app is almost done It's just ugly I need to do some styling and then I will push it

---
+++ app.js
@@ -0,0 +1,59 @@
+
+
+
+class DOM {
+ test = document.querySelector('.test')
+ country = document.querySelector('.countryname')
+ cityname = document.querySelector('.cityname')
+ temperature = document.querySelector('.temperature')
+ humidity = document.querySelector('.humidity')
+ city = "Delhi India"
+ input = document.querySelector("#city")
+ submitbtn = document.querySelector('button')
+}
+
+let dom = new DOM();
+
+async function apiCall(url) {
+    try {
+        const response = await fetch(url , {mode:'cors'});
+        const data = await response.json();
+        const main_data = extractData(data)
+        domchange(main_data)
+    } catch(error) {
+        console.log(`There is an error ${error}`);
+    }
+}
+
+function domchange(obj) {
+    dom.country.textContent = obj.country_name
+    dom.cityname.textContent = obj.city_name
+    dom.temperature.textContent = `${obj.city_temp_c}°C`
+    dom.humidity.textContent = `${obj.city_humidity}%`
+}
+
+function formdata() {
+    dom.submitbtn.addEventListener('click' , function(e) {
+        e.preventDefault()
+        dom.city = dom.input.value 
+        apiCall(`http://api.weatherapi.com/v1/current.json?key=1b99b3dfb10948888fe125556233007&q=${dom.city}`)
+        dom.input.value = '';
+    })
+}
+
+
+
+function extractData(jsonData) {
+    var weather = {
+        city_name: jsonData.location.name , 
+        country_name: jsonData.location.country,
+        city_temp_c: jsonData.current.temp_c,
+        city_temp_f: jsonData.current.temp_f,
+        city_humidity: jsonData.current.humidity,
+    }
+
+    return weather
+}
+
+
+formdata()
