c5f5efa4ef8da727b24154775f081e52c3dad9ae
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2023-08-13 12:24:02 +0530

    Now New Delhi's weather will be displayed at the start of the application

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 
-
 class DOM {
- city = "Los Angeles"
- input = document.querySelector("#city")
- submitbtn = document.querySelector('button')
- city_name = document.querySelector('.city-name');
- temperature = document.querySelector('.temp');
- condition = document.querySelector('.condition');
- humidity = document.querySelector('.humidity');
- times = document.querySelectorAll('.time');
- icons = document.querySelectorAll('.condition-icon')
- timetemps = document.querySelectorAll('.time-temp');
- mainimage = document.querySelector('.main-image')
+    city = "Los Angeles"
+    input = document.querySelector("#city")
+    submitbtn = document.querySelector('button')
+    city_name = document.querySelector('.city-name');
+    temperature = document.querySelector('.temp');
+    condition = document.querySelector('.condition');
+    humidity = document.querySelector('.humidity');
+    times = document.querySelectorAll('.time');
+    icons = document.querySelectorAll('.condition-icon')
+    timetemps = document.querySelectorAll('.time-temp');
+    mainimage = document.querySelector('.main-image')
 }
 
 let dom = new DOM();
+start();
 console.log(dom.mainimage)
 async function apiCall(url) {
     try {
-        const response = await fetch(url , {mode:'cors'});
+        const response = await fetch(url, { mode: 'cors' });
         const data = await response.json();
         console.log(data)
         let main_data = extractData(data)
         console.log(main_data);
         domchange(main_data)
-    } catch(error) {
+    } catch (error) {
         console.log(`There is an error ${error}`);
     }
 }
@@ -38,25 +38,25 @@ function domchange(obj) {
 }
 
 function formdata() {
-    dom.submitbtn.addEventListener('click' , function(e) {
+    dom.submitbtn.addEventListener('click', function (e) {
         e.preventDefault()
-        dom.city = dom.input.value 
+        dom.city = dom.input.value
         apiCall(`http://api.weatherapi.com/v1/current.json?key=1b99b3dfb10948888fe125556233007&q=${dom.city}`);
-        forecastAPICall(`http://api.weatherapi.com/v1/forecast.json?key=1b99b3dfb10948888fe125556233007&q=${dom.city}&days=7`);        
+        forecastAPICall(`http://api.weatherapi.com/v1/forecast.json?key=1b99b3dfb10948888fe125556233007&q=${dom.city}&days=7`);
         dom.input.value = '';
     })
 }
 
 function extractData(jsonData) {
     var weather = {
-        city_name: jsonData.location.name , 
+        city_name: jsonData.location.name,
         country_name: jsonData.location.country,
         city_temp_c: jsonData.current.temp_c,
         city_temp_f: jsonData.current.temp_f,
         city_humidity: jsonData.current.humidity,
-        condition:jsonData.current.condition.text,
-        icon:jsonData.current.condition.icon,
-        uv:jsonData.current.uv,
+        condition: jsonData.current.condition.text,
+        icon: jsonData.current.condition.icon,
+        uv: jsonData.current.uv,
     }
 
     return weather
@@ -64,20 +64,20 @@ function extractData(jsonData) {
 
 async function forecastAPICall(url) {
     try {
-        const response = await fetch(url , {mode:'cors'})
+        const response = await fetch(url, { mode: 'cors' })
         const data = await response.json();
         let forecastdata = extractForecastData(data)
         console.log(forecastdata)
         domchangeforecast(forecastdata)
 
-    } catch(error) {
+    } catch (error) {
         console.log(error)
     }
 }
 
 
 function domchangeforecast(obj) {
-    for(let i = 0 ; i < dom.times.length; i++) {
+    for (let i = 0; i < dom.times.length; i++) {
         dom.times[i].textContent = obj["0"].hour[i].time;
         dom.icons[i].src = obj["0"].hour[i].icon
         dom.timetemps[i].textContent = `${obj["0"].hour[i].hour_temp_c}°C`
@@ -86,23 +86,39 @@ function domchangeforecast(obj) {
 
 function extractForecastData(jsonData) {
     forecast = {}
-    for(let i = 0 ; i < 7 ; i++) {
-        
-        forecast[i] = {date: jsonData.forecast.forecastday[i].date 
+    for (let i = 0; i < 7; i++) {
+
+        forecast[i] = {
+            date: jsonData.forecast.forecastday[i].date
             , max_temp_c: jsonData.forecast.forecastday[i].day.maxtemp_c,
             min_temp_c: jsonData.forecast.forecastday[i].day.mintemp_c,
-        hour: [],avghumidity: jsonData.forecast.forecastday[i].day.avghumidity 
-        , chance_of_rain: jsonData.forecast.forecastday[i].day.daily_chance_of_rain , chance_of_snow: jsonData.forecast.forecastday[i].day.daily_chance_of_snow,
-        condition: jsonData.forecast.forecastday[i].day.condition.text , icon:jsonData.forecast.forecastday[i].day.condition.icon,}
+            hour: [], avghumidity: jsonData.forecast.forecastday[i].day.avghumidity
+            , chance_of_rain: jsonData.forecast.forecastday[i].day.daily_chance_of_rain, chance_of_snow: jsonData.forecast.forecastday[i].day.daily_chance_of_snow,
+            condition: jsonData.forecast.forecastday[i].day.condition.text, icon: jsonData.forecast.forecastday[i].day.condition.icon,
+        }
 
-        for (let j = 0 ; j < 24 ; j++) {
-            forecast[i].hour[j] = {time: jsonData.forecast.forecastday[i].hour[j].time,
-            hour_temp_c: jsonData.forecast.forecastday[i].hour[j].temp_c , 
-            hour_humidity: jsonData.forecast.forecastday[i].hour[j].humidity , condition: jsonData.forecast.forecastday[i].hour[j].condition.text , 
-            icon: jsonData.forecast.forecastday[i].hour[j].condition.icon,}
+        for (let j = 0; j < 24; j++) {
+            forecast[i].hour[j] = {
+                time: jsonData.forecast.forecastday[i].hour[j].time,
+                hour_temp_c: jsonData.forecast.forecastday[i].hour[j].temp_c,
+                hour_humidity: jsonData.forecast.forecastday[i].hour[j].humidity, condition: jsonData.forecast.forecastday[i].hour[j].condition.text,
+                icon: jsonData.forecast.forecastday[i].hour[j].condition.icon,
+            }
         }
     }
 
     return forecast
 }
+
+function start() {
+    apiCall(`http://api.weatherapi.com/v1/current.json?key=1b99b3dfb10948888fe125556233007&q=New+Delhi`);
+    forecastAPICall(`http://api.weatherapi.com/v1/forecast.json?key=1b99b3dfb10948888fe125556233007&q=New+Delhi&days=7`);
+
+
+}
+
+
+
+
+
 formdata()
